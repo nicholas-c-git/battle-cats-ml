8a029d9b1ca950ac0373d0b0d5d89cdcab86917d
nicholas-c-git <nich@hawaii.edu>
2026-08-27 22:25:30 -1000

    decided to pivot to predicting a singular banner first, mostly just getting things started

--- predict_epicfest.py
+++ predict_epicfest.py
@@ -1,10 +1,9 @@
 import pandas as pd
 import numpy as np
 
-#probably not enough data to do a good split, probably won't use
 from sklearn.model_selection import train_test_split
 
-#used to encode series id into a bunch of binary features
+#encoding series id to either 1 for epicfest, or 0
 from sklearn.preprocessing import OneHotEncoder
 
 #basic classification models
@@ -19,17 +18,16 @@ data_important = pd.read_csv('series.csv', parse_dates=[0,1],date_format='%Y-%m-
 one_hot = OneHotEncoder(sparse_output=False).set_output(transform='pandas')
 
 #make a DataFrame of the series id column but encoded
+#series id 27 is epicfest
 data_encoded = one_hot.fit_transform(data_important[['series id']])
 
 #making a dataframe of the features to be used
 data_compiled = pd.concat(
     [data_important[['start date', 'end date']],
-     data_encoded],
+     data_encoded[['series id_27']]],
     axis=1) #columns
 
 print(data_compiled)
 #probably going to transform the 'start date' (YYYY-MM-DD) into just month and day or
 #time since start of year.
 #also want to turn 'end date' into a duration instead (start date - end date)
-
-#I'll work on this file after I finish predict_epicfest.py
