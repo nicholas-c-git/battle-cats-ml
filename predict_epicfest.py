6f289f7b71c9bb7a2667f68ed2afdbccd59b576b
nicholas-c-git <nich@hawaii.edu>
2026-08-28 09:30:17 -1000

    improved readability

--- predict_epicfest.py
+++ predict_epicfest.py
@@ -12,22 +12,26 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
-data_important = pd.read_csv('series.csv', parse_dates=[0,1],date_format='%Y-%m-%d')
+data_important = pd.read_csv('series.csv')
 
 #initialize a one hot encoder in pandas DataFrame format
 one_hot = OneHotEncoder(sparse_output=False).set_output(transform='pandas')
 
 #make a DataFrame of the series id column but encoded
+#convert_dtypes because the 1s and 0s were 1.0 and 0.0
+data_encoded = one_hot.fit_transform(data_important[['series id']]).convert_dtypes()
+
 #series id 27 is epicfest
-data_encoded = one_hot.fit_transform(data_important[['series id']])
+data_27 = data_encoded[['series id_27']]
 
 #making a dataframe of the features to be used
 data_compiled = pd.concat(
     [data_important[['start date', 'end date']],
-     data_encoded[['series id_27']]],
+     data_27],
     axis=1) #columns
 
 print(data_compiled)
 #probably going to transform the 'start date' (YYYY-MM-DD) into just month and day or
 #time since start of year.
 #also want to turn 'end date' into a duration instead (start date - end date)
+
