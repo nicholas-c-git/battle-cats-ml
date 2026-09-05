207774846c1b77d15bda2b4550e01308f2a65040
nicholas-c-git <nich@hawaii.edu>
2026-09-04 22:38:00 -1000

    improved comments, also added pseudocode for the next input feature to implement

--- prepare_training_data.py
+++ prepare_training_data.py
@@ -5,13 +5,20 @@ import numpy as np
 from sklearn.preprocessing import OneHotEncoder
 
 #you can make series_id.csv using convert_series_id.py
-data_important = pd.read_csv('series_id.csv').sort_values('start date')
+#sorted by 'series id' then 'start date'
+series_data = pd.read_csv('series_id.csv')
 
 #date columns as a datetime
-start_date = pd.to_datetime(data_important['start date'])
-end_date = pd.to_datetime(data_important['end date'])
+start_date = pd.to_datetime(series_data['start date']) #YYYY-MM-DD
+end_date = pd.to_datetime(series_data['end date']) #YYYY-MM-DD
 
-#making a dataframe of most of the features
+#make 'days since last appearance' here
+#for every item in series id
+#   if it's not the first
+#       value is difference between this and previous's start dates
+print(series_data)
+
+#making a dataframe of most of the important features
 data_compiled = pd.concat(
     [start_date.dt.month,
      start_date.dt.day,
@@ -19,7 +26,7 @@ data_compiled = pd.concat(
      start_date - pd.to_datetime(start_date.dt.year.astype(str) + "-01-01"), #days since start of year
      ], axis=1) #join columns
 
-#rename columns and convert seconds to days
+#rename columns and convert number of seconds to number of days
 data_compiled.columns = ['start month', 'start day', 'duration', 'days since Jan 1']
 data_compiled['duration'] = data_compiled['duration'].dt.days
 data_compiled['days since Jan 1'] = data_compiled['days since Jan 1'].dt.days
@@ -29,9 +36,10 @@ one_hot = OneHotEncoder(sparse_output=False).set_output(transform='pandas')
 
 #make a DataFrame of the encoded series ids columns
 #convert_dtypes because the 1s and 0s were 1.0 and 0.0
-data_encoded = one_hot.fit_transform(data_important[['series id']]).convert_dtypes()
+data_encoded = one_hot.fit_transform(series_data[['series id']]).convert_dtypes()
 
 data_compiled = pd.concat([data_compiled, data_encoded], axis=1)
 
-#the input features
+#output the input features to a csv
+#still sorted by 'series id' then 'start date'
 data_compiled.to_csv('training_data.csv')
